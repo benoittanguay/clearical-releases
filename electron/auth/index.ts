0e50cb4f72e219be2189fd0b6b2302e3991b848c
benoittanguay <benoit.tanguay.design@gmail.com>
2026-01-31 21:34:22 -0500

    Set up gh-pages branch for marketing site

--- electron/auth/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Auth Module
- *
- * Exports for the Supabase authentication system.
- */
-
-export { SupabaseAuthService, getAuthService } from './supabaseAuth.js';
-export type { AuthUser, AuthSession, AuthResult } from './supabaseAuth.js';
-export { initializeAuth } from './ipcHandlers.js';
--- prototypes/notch-test/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    hideWidget: () => ipcRenderer.send('widget:hide'),
-    stopRecording: () => ipcRenderer.send('widget:stop'),
-    onShow: (callback) => ipcRenderer.on('widget:show', callback)
-});
